5f1f672b1cf4854f932d266dcc71e4bbf8bd67cf
Frank-W-B <frank.burkholder@gmail.com>
2020-05-08 22:26:10 -0600

    Add docstrings

--- src/text_parsing_functions.py
+++ src/text_parsing_functions.py
@@ -1,37 +1,159 @@
 from string import punctuation
-from sklearn.feature_extraction import stop_words
-stopwords = stop_words.ENGLISH_STOP_WORDS
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+stopwords = ENGLISH_STOP_WORDS
+
 
 def lowercase_text(text):
+    '''Returns a text string with all characters lower-cased.
+
+    Parameters
+    ----------
+    text: str
+
+    Returns
+    -------
+    text_lowercased: str
+
+    Examples
+    --------
+    >>> lowercase_text('AbC')
+   'abc'
+    '''
     pass
+
 
 def remove_punctuation(text, punctuation=punctuation):
+    '''Returns a text string without punctuation.
+
+    Parameters
+    ----------
+    text: str
+    punctuation: str
+        A string containing all the punctuation characters to remove.
+
+    Returns
+    -------
+    text_nopunct: str
+
+    Examples
+    --------
+    >>> remove_punctuation("here's johnny!")
+    'heres johnny'
+    '''
     pass 
 
+
 def remove_newline(text):
-    pass
+    '''Removes all newlines in a line of text
+
+    Parameters
+    ----------
+    text: str
+
+    Returns
+    -------
+    text_no_nl: str
+
+    Examples
+    --------
+    >>> remove_newline("\nlife happens when youre busy\n making other plans\n")
+    'life happens when youre busy making other plans'
+    '''
+    pass 
+
 
 def split_text_into_words(text):
-    pass
+    '''Splits a text string into a word list
+
+    Parameters
+    ----------
+    text: str
+
+    Returns
+    -------
+    words: list of str
+
+    Examples
+    --------
+    >>> split_text_into_words("get started by stop talking and begin doing")
+    ['get', 'started', 'by', 'stop', 'talking', 'and', 'begin', 'doing']
+    '''
+    pass 
+
 
 def remove_stopwords(word_lst, stopwords_set):
-    pass
+    '''Removes words from word_lst if in the stopwords_set
+
+    Parameters
+    ----------
+    word_lst: list of str
+    stopwords_set: set of str
+
+    Returns
+    -------
+    word_lst_no_sw: list of str
+
+    Examples
+    --------
+    >>> remove_stopwords(['tell', 'me', 'and', 'i', 'forget'], set(['and', 'i']))
+    ['tell', 'me', 'forget']
+    '''
+    pass 
+
 
 def replace_names(word_lst, name_set, replacement_val):
+    '''Replaces names in word_lst with replacement_val.  Names are identified in
+    the name set.
+
+    Parameters
+    ----------
+    word_lst: list of str
+    name_set: set of str
+    replacement_val: str
+        The string to replace the names with.
+
+    Returns
+    -------
+    word_lst_replaced_names: list of str
+
+    Examples
+    --------
+    >>> replace_names(['daryl', 'daryl'], set(['larry', 'darryl']), 'person')
+    ['person', 'person']
+    '''
     pass
+
 
 def create_cleaned_textline_from_words(words):
+    '''Makes a single string from a list of words.
+
+    Parameters
+    ----------
+    words: list of str
+
+    Returns
+    -------
+    cleaned_text: str
+
+    Examples
+    --------
+    >>> create_cleaned_textline_from_words(['darkest', 'moments', 'focus', 'light'])
+    'darkest moments focus light'
+    '''
     pass
 
+
 def line_cleaning_pipeline(text, stopwords_set, name_set, replace_val):
+    '''Transforms raw text into clean text using text-cleaning functions above'''
     text_lc = lowercase_text(text)
     text_np = remove_punctuation(text_lc)
     text_nnl = remove_newline(text_np)
     words = split_text_into_words(text_nnl)
     words_nsw = remove_stopwords(words, stopwords_set)
-    words_cleaned = replace_names(words_nsw, name_set, replace_val) 
+    words_cleaned = replace_names(words_nsw, name_set, replace_val)
     line_of_text_cleaned = create_cleaned_textline_from_words(words_cleaned)
     return line_of_text_cleaned
+
 
 if __name__ == '__main__':
     # to help test functions and pipeline:
@@ -40,4 +162,3 @@ if __name__ == '__main__':
     text_str3 = "to take her to Busan to see her mother. \nThey board the KTX at Seoul Station."
 
     # your code below
-
